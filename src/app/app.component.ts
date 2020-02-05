832a387e53c898eac24942f78bdf9f35c5ae5bf7
saumitrakahate <saumitra.kahate@gmail.com>
2020-02-05 08:44:12 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+serverElements = [{type: 'Server', name: 'Testserver',content: 'Some test server'}];
+}
