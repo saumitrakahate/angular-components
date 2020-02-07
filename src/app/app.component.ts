fb166e339e638119b225980a20c94584c2fd991c
saumitrakahate <saumitra.kahate@gmail.com>
2020-02-07 08:47:10 +0530

    Lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,12 @@ onBlueprintAdded(blueprintData :{serverName: string, serverContent: string}) {
     content: blueprintData.serverContent
   });
 }
+
+onChangeFirst(){
+  this.serverElements[0].name = 'Changed';
+}
+
+onDestroyFirst(){
+  this.serverElements.splice(0,1);
+}
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy, ViewChild, ElementRef, ContentChild } from '@angular/core';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 
 @Component({
   selector: 'app-server-element',
@@ -6,13 +7,61 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation : ViewEncapsulation.Emulated //Native, None
 })
-export class ServerElementComponent implements OnInit {
-
-  @Input('srvElement') element : {type: string, name: string, content: string};
+export class ServerElementComponent implements 
+OnInit,
+OnChanges,
+DoCheck, 
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
   
-  constructor() { }
+  @Input('srvElement') element : {type: string, name: string, content: string};
 
+  @Input() name: string;
+
+  @ViewChild('heading',{static:true}) header : ElementRef;
+
+  @ContentChild('para',{static:true}) paraContent : ElementRef;
+  
+  constructor() {
+    console.log('Constructor called');
+   }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+   
   ngOnInit() {
+    console.log('ngOnInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log('Para content: ' + this.paraContent.nativeElement.textContent);
   }
 
+  ngDoCheck(){
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+    console.log('Para content: ' + this.paraContent.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit called');
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called');
+  }
 }
